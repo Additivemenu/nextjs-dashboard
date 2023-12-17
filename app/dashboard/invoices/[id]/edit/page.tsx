0cb217a6ee6bca0a53cac5c48d311334f0ab5218
XUeshuo Li <spoonlee24k@gmail.com>
2023-12-17 21:55:47 +1100

    C14 error handling done

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
   // ! params is an alternative to searchParams to get url state info
@@ -10,6 +10,10 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound(); // ! error handling
+  }
 
   return (
     <main>
